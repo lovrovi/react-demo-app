4010201dfac2bd80a4f239a63f5abfb7e33f7811
lovrovidovic <lovrovidovic6@gmail.com>
2021-11-01 10:28:36 +0100

    added portals for modal and backdrop

--- src/components/Modals/Modal.js
+++ src/components/Modals/Modal.js
@@ -1,28 +1,49 @@
 import React from "react";
+import ReactDom from "react-dom";
 import { Card } from "../Containers/Card";
 import styles from "./Modal.module.css";
 
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={() => props.action("")} />;
+};
+
+const ErrorModal = (props) => {
+  return (
+    <Card style={styles.modal}>
+      <div className={styles.header}>
+        <h2>{props.title}</h2>
+      </div>
+
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+
+      <div className={styles.actions}>
+        <button onClick={() => props.action("")}>Okay</button>
+      </div>
+    </Card>
+  );
+};
+
 export const Modal = (props) => {
-  const clickHandler = () => {
+  /* const clickHandler = () => {
     props.setShowModal("");
-  };
+  }; */
 
   return (
-    <div>
-      <div className={styles.backdrop} onClick={clickHandler}/>
-      <Card style={styles.modal}>
-        <div className={styles.header}>
-          <h2>{props.title}</h2>
-        </div>
-
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-
-        <div className={styles.actions}>
-          <button onClick={clickHandler}>Okay</button>
-        </div>
-      </Card>
-    </div>
+    <>
+      {ReactDom.createPortal(
+        <Backdrop action={props.setShowModal} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDom.createPortal(
+        <ErrorModal
+          title={props.title}
+          message={props.message}
+          action={props.setShowModal}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </>
   );
 };
